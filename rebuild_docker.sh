87f70e5f5ea27c9722fc8db638792e55d3c8d287
Jacob Hammack <jacob.hammack@hammackj.com>
2025-06-22 04:14:12 -0500

    Tweaks

--- rebuild_docker.sh
+++ rebuild_docker.sh
@@ -4,7 +4,8 @@ export DOCKER_BUILDKIT=1
 
 git pull &&
 docker-compose down &&
-docker-compose build --no-cache app &&
+#docker-compose build --no-cache app &&
+docker-compose build app &&
 docker-compose up -d &&
 docker-compose logs app &&
 docker ps
