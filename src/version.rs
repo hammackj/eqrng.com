17caad03e05379a59475479c10563ff9a9ade696
Jacob Hammack <jacob.hammack@hammackj.com>
2025-07-19 22:32:51 -0500

    Added a version endpoint, this is used to display the footer

---
+++ src/version.rs
@@ -0,0 +1,23 @@
+use axum::Json;
+use serde::Serialize;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const UPDATE_VERSION: &str = "Update 3 - July 19, 2025";
+
+#[derive(Serialize)]
+pub struct VersionResponse {
+    version: &'static str,
+}
+
+pub async fn version() -> Json<VersionResponse> {
+    let version = format!(
+        "v{} - {}",
+        String::from(VERSION),
+        String::from(UPDATE_VERSION)
+    );
+    let static_version: &'static str = Box::leak(version.into_boxed_str());
+
+    Json(VersionResponse {
+        version: static_version,
+    })
+}
